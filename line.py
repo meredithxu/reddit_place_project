03e2923af6dbf9cd6c23d78ec81f2e2417d0c2ee
Daniel Shu <danielshu98@gmail.com>
2019-01-13 15:35:40 -0800

    modularized points, lines, and path

---
+++ line.py
@@ -0,0 +1,45 @@
+
+
+class Line:
+  """
+    Line objects represent each straight edge of a Path
+    The Lines consist of two points, the start and end points
+  """
+
+  def __init__(self, point1, point2):
+    self.point1 = point1
+    self.point2 = point2
+
+  def set_point_1(self, x, y):
+    self.point1 = Point(x,y)
+
+  def set_point_2(self, x, y):
+    self.point1 = Point(x,y)
+
+  def get_point1(self):
+    return self.point1
+
+  def get_point2(self):
+    return self.point2
+
+  def point_is_on_leftside_of_line(self, point):
+    """
+      Given coordinates (x,y), check whether drawing a horizontal line rightward from the point will cross this Line object
+      Return True if so and False if not
+    """
+
+    if (point.y <= self.point2.y and point.y >= self.point1.y) or (point.y >= self.point2.y and point.y <= self.point1.y):
+
+      # The equation to determine this comes from here: https://math.stackexchange.com/questions/274712/calculate-on-which-side-of-a-straight-line-is-a-given-point-located
+
+      d = ((point.x - self.point1.x)*(self.point2.y - self.point1.y)) - ((point.y - self.point1.y)*(self.point2.x - self.point1.x))
+
+      left = (- 1)*(self.point2.y - self.point1.y)
+      # If d and left are both positive or both negative, then the point is on the left side of the line
+
+      if (d < 0 and left < 0) or (d > 0 and left > 0):
+        return True
+
+
+      else:
+        return False
--- reddit_tester.py
+++ reddit_tester.py
@@ -1,48 +1,117 @@
+from line import *
+from point import *
+from path import *
+import csv
+import re
 from reddit import *
 
+# extract path values from JSON file and store them in a dictionary whose key is the picture ID(string) and value is a list of coordinates(string) indicating the location of a picture
+
+# locations = dict()
+# read = False
+# numOfPic = 0
+# picId = -1
+# with open('atlasTest2.js') as atlasJS:
+#   for line in atlasJS:
+#     if '"id":' in line:
+#       picId = re.findall('\d+',line)[0]
+#       #print(picId)
+#     if '"path"' in line:
+#       read = True
+#       numOfPic = numOfPic + 1
+#       #print(numOfPic)
+#     if read:
+#       if len(locations) < numOfPic:
+#         l = []
+#         locations[picId] = l
+#         coord = re.findall(r"[-+]?[0-9]*\.?[0-9]+",line)
+#         for co in coord:
+#           locations.get(picId).append(co)
+#     else:
+#       coord = re.findall(r"[-+]?[0-9]*\.?[0-9]+",line)
+#       #print(coord)
+#       if picId not in locations:
+#         print("ERROR!!!!!!!!!!")
+#         print(picId)
+#       for co in coord:
+#         locations.get(picId).append(co)
+#     if "}" in line:
+#        read = False
+
+locations = store_locations()
+
+# Add a simple triangle for testing purposes
+
+locations['triangle'] = ['0','0','3','3','3','0']
+
+
+# Parse the data into the point, line, and path objects
+for pic_id in locations:
+  path = Path(pic_id)
+  path_points = locations.get(pic_id)
+  i = 0
+  while i < len(path_points):
+    start_x = float(path_points[i])
+    start_y = float(path_points[i + 1])
+    end_x = float(path_points[0])
+    end_y = float(path_points[1]) 
+    if i != len(path_points) - 2:
+        end_x = float(path_points[i + 2])
+        end_y = float(path_points[i + 3])
+
+    point1 = Point(start_x, start_y)
+    point2 = Point(end_x, end_y)
+    line = Line(point1, point2)
+    path.add_line(line)
+    i += 2
+
+  # Replace the list of numbers with the Path object
+  locations[pic_id] = path
 
 # Every pixel within square (838,415), (838,483), (886, 486), and (886,415) should be a part of the image 
-locations = store_locations()
 first_image_path = locations.get('0')
-print(first_image_path)
+# print(first_image_path)
 for x in range(839, 887):
   for y in range(416, 484):
-    if not pixel_is_in_image(x,y, first_image_path):
+    test_point = Point(x,y)
+    if not first_image_path.pixel_is_in_image(test_point):
       print("FAILED " + str(x) + "," + str(y))
 
 #Every pixel not in this square should not be a part of the image
 
 for x in range(0, 839):
   for y in range(0, 416):
-    if pixel_is_in_image(x,y, first_image_path):
+    test_point = Point(x,y)
+    if first_image_path.pixel_is_in_image(test_point):
       print("FAILED " + str(x) + "," + str(y))
 
 for x in range(887, 1000):
   for y in range(484, 1000):
-    if pixel_is_in_image(x,y, first_image_path):
+    test_point = Point(x,y)
+    if first_image_path.pixel_is_in_image(test_point):
       print("FAILED " + str(x) + "," + str(y))    
 
 
 
-triangle = ['0','0','3','3','3','0']
 
+triangle = locations.get('triangle')
 #Test that pixel_is_in_image correctly returns true for points within across a diagonal
-if pixel_is_in_image(1, 2, triangle):
+if triangle.pixel_is_in_image(Point(1, 2)):
   print("FAILED 1, 2")
-if not pixel_is_in_image(2, 1, triangle):
+if not triangle.pixel_is_in_image(Point(2, 1)):
   print("FAILED 2, 1")
 
 
 italyFlag = locations.get('13')
 
 # Test that pixel_is_in_image correctly returns false when given coordinates within a hole in the middle of the polygon
-if pixel_is_in_image(135, 359, italyFlag):
+if italyFlag.pixel_is_in_image(Point(135, 359)):
   print("FAILED 135, 359")
 
-if pixel_is_in_image(130, 355, italyFlag):
+if italyFlag.pixel_is_in_image(Point(130, 355)):
   print("FAILED 130,355")
 
-if pixel_is_in_image(120, 360, italyFlag):
+if italyFlag.pixel_is_in_image(Point(120, 360)):
   print("FAILED 120,360")
 
 
