589643ff620a3859346c870c86e1d01de811fea4
Meredith Xu <meredithxu@Merediths-MacBook-Pro.local>
2019-01-17 19:50:21 -0800

    spatial sub data done

--- reddit.py
+++ reddit.py
@@ -1,6 +1,8 @@
 import csv
 import re
-
+from line import *
+from point import *
+from path import *
 
 # a helper function that organizes coordinates retrieved from JSON file as list of tuples given an ID
 def coordHelper(picId,locations):
@@ -51,6 +53,34 @@ def store_locations():
     print(numOfPic)
     return locations
 
+
+# Given the vertices of a rectangle, return data points inside that rectangle
+def spatialData(all_data, lo_left_v, lo_right_v, up_left_v, up_right_v):
+    left_line = Line(lo_left_v,up_left_v)
+    right_line = Line(lo_right_v,up_right_v)
+    up_line = Line(up_left_v,up_right_v)
+    lo_line = Line(lo_left_v,lo_right_v)
+    rectangle = Path(-1)
+    rectangle.add_line(left_line)
+    rectangle.add_line(right_line)
+    rectangle.add_line(up_line)
+    rectangle.add_line(lo_line)
+    spatial_sub = []
+
+    # iterate through the dictionary. For each picture, check if every coordinate of that picture is inside the rectangle. If so, add picture ID to the subdata
+    for key, value in all_data.items():
+        coords = coordHelper(key,all_data)
+        in_image = True
+        for co in coords:
+            p = Point(float(co[0]),float(co[1]))
+            if not rectangle.pixel_is_in_image(p):
+                in_image = False
+                break
+        if in_image:
+            spatial_sub.append(key)
+    return spatial_sub
+
+
 if __name__ == "__main__":
 
     subData = []
@@ -81,7 +111,7 @@ if __name__ == "__main__":
     for pId in range(21):
         coordinates=coordHelper(str(pId),locations)
         print(coordinates)
-
-
+    print("TEST SUB DATA!!!!!!!!!!!!!!!!")
+    print(spatialData(locations,Point(500,0),Point(500,500),Point(0,0),Point(0,500)))
 
 
