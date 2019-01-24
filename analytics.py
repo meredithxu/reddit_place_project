784e5d52160c8ada15e378232eab3149561d07d0
Daniel Shu <danielshu98@gmail.com>
2019-01-23 18:24:31 -0800

    added time analysis

--- analytics.py
+++ analytics.py
@@ -1,4 +1,5 @@
 import csv
+import sys
 from reddit import *
 from line import *
 from point import *
@@ -50,9 +51,54 @@ def write_users_per_project(locations, filename, write_to_file = False):
 
   return user_count
 
+
+def get_time_per_project(locations, filename, write_to_file = True):
+  # Find the oldest and the newest point of every project and return them as a tuple
+  times_count = {}
+
+  for pic_id in locations:
+    path = locations.get(pic_id)
+    min_time = sys.maxsize
+    max_time = -sys.maxsize
+    with open(filename,'r') as file:
+      # Skip first line (header row)
+      next(file, None)
+
+      reader = csv.reader(file)
+      
+      for r in reader:
+        time = int(r[0])
+        x = float(r[2])
+        y = float(r[3])
+
+        # If this pixel is inside the image, then this user has contributed to the image
+        if ( path.pixel_is_in_image(Point(x,y))):
+          if time > max_time:
+            max_time = time
+          if time < min_time:
+            min_time = time
+
+
+    time_alive = max_time - min_time
+
+    times_count[pic_id] = (min_time, max_time, time_alive)
+
+  if write_to_file:
+    # Write these results in a CSV file
+    with open('time_per_project.csv','w') as fileOut:
+      writer = csv.writer(fileOut, delimiter = ",")
+      writer.writerow(["Pic ID", "Start Time", "End Time", "Duration"])
+      for pic_id in times_count:
+        writer.writerow([pic_id, times_count.get(pic_id)[0], times_count.get(pic_id)[1], times_count.get(pic_id)[2]])
+
+
+  return times_count
+
 if __name__ == "__main__":
   locations = store_locations()
+  print(locations)
   write_users_per_project(locations, "tile_placements_sub.csv", True)
+  get_time_per_project(locations, "tile_placements_sub.csv", True)
 
 
 
