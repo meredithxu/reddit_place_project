23ef32072a23b99dc514ae1c736976666a27df56
Daniel Shu <danielshu98@gmail.com>
2020-02-23 16:15:24 -0800

    updated model

--- Python_code/nonlinear_regressor.py
+++ Python_code/nonlinear_regressor.py
@@ -1,20 +1,9 @@
 
 import numpy
-import matplotlib.pyplot as plt
-from sklearn.metrics import r2_score
-import tensorflow as tf
-from tensorflow import keras
-from tensorflow.keras import layers
-from tensorflow.keras import regularizers
-from keras.layers import Dense
+
+from keras.layers import Dense, Dropout, Activation
 from keras.models import Sequential
 import numpy as np
-
-
-# To install tensorflow_docs: pip install git+https://github.com/tensorflow/docs
-import tensorflow_docs as tfdocs
-import tensorflow_docs.plots
-import tensorflow_docs.modeling
 
 
 def createNonlinearRegressionNeuralNet(A, b):
@@ -22,29 +11,31 @@ def createNonlinearRegressionNeuralNet(A, b):
         Given a matrix A and set of labels b, create and return a
         nonlinear regression neural net
     '''
-
-    dataset = np.array(A), np.array(b)
-
-    train_dataset = dataset.sample(frac=0.8, random_state=0)
-    test_dataset = dataset.drop(train_dataset.index)
-
-    train_data = train_dataset[0]
-    train_labels = train_dataset[1]
-
-    test_data = test_dataset[0]
-    test_labels = test_dataset[1]
+    dims = A.shape[1] # Number of columns
 
     # create model
     model = Sequential()
-    model.add(Dense(20, activation="tanh", input_dim=5,
-                    kernel_initializer="uniform"))
-    model.add(Dense(1, activation="linear", kernel_initializer="uniform"))
 
-    # Compile model
-    model.compile(loss='mse', optimizer='adam', metrics=['accuracy'])
+    # Input layer with dimension dims and hidden layer i with 128 neurons.
+    model.add(Dense(128, input_dim=dims, activation='relu'))
+    # Dropout of 20% of the neurons and activation layer.
+    model.add(Dropout(.2))
+    model.add(Activation("linear"))
+    # Hidden layer j with 64 neurons plus activation layer.
+    model.add(Dense(64, activation='relu'))
+    model.add(Activation("linear"))
+    # Hidden layer k with 64 neurons.
+    model.add(Dense(64, activation='relu'))
+    # Output Layer.
+    model.add(Dense(1))
 
-    # Fit the model
-    model.fit(train_data, train_labels, epochs=1000, batch_size=10,  verbose=2)
+    # Model is derived and compiled using mean square error as loss
+    # function, accuracy as metric and gradient descent optimizer.
+    model.compile(loss='mse', optimizer='adam', metrics=["accuracy"])
+
+    # Training model with train data. Fixed random seed:
+    numpy.random.seed(3)
+    model.fit(A, b, nb_epoch=256, batch_size=2, verbose=2)
 
     return model
 
